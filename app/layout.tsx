e33d25b20f243df321f5f7c7e7c8b0c155089bfa
Bison Hubert <bisonhubert@gmail.com>
2025-05-21 13:31:45 -0700

    Add urql provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
+import { GQLProvider } from './gqlProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
